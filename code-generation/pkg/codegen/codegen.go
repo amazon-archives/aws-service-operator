d7704b0ede78f4c5ec26b77a739617f15e20d8a4
Christopher Hein <me@christopherhein.com>
2018-10-06 08:57:07 +0000

    Adding Code Generate source files
    Signed-off-by: Christopher Hein <me@christopherhein.com>

--- code-generation/pkg/codegen/codegen.go
+++ code-generation/pkg/codegen/codegen.go
@@ -122,7 +122,7 @@ func (c *Codegen) Run() error {
 			if (Customizations{}) == parsedModel.Spec.Customizations {
 				createFile(rootPath, "cft.go", "cft.go", operatorPath+"/", parsedModel)
 			}
-			createFile(rootPath, "controller.go", "controller.go", operatorPath+"/", parsedModel)
+			createFile(rootPath, "operator.go", "operator.go", operatorPath+"/", parsedModel)
 
 			createFile(rootPath, parsedModel.Spec.Resource.Name+".go", "types.go", apiPath+"/", parsedModel)
 
@@ -130,8 +130,10 @@ func (c *Codegen) Run() error {
 	}
 
 	helpersPath := rootPath + "pkg/helpers"
+	configPath := rootPath + "configs"
 
 	createFile(rootPath, "template_functions.go", "template_functions.go", helpersPath+"/", models)
+	createFile(rootPath, "aws-service-operator.yaml", "aws-service-operator.yaml", configPath+"/", models)
 
 	return nil
 }
@@ -188,9 +190,12 @@ func createFile(rootPath string, fileName string, templateName string, path stri
 		return
 	}
 
-	formatted, err := format.Source(bf.Bytes())
-	if err != nil {
-		log.Fatalf("Error formatting resolved template: %+v", err)
+	formatted := bf.Bytes()
+	if templateName[len(templateName)-3:] == ".go" {
+		formatted, err = format.Source(bf.Bytes())
+		if err != nil {
+			log.Fatalf("Error formatting resolved template: %+v", err)
+		}
 	}
 
 	f, err := os.Create(path + fileName)
