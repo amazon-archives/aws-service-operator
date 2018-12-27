31dd9f0457a32955d18d3698c59a4f7b60774129
Zane Williamson <zanew@zillow.com>
2018-12-27 10:45:06 -0800

    adding elasticache non-gen code

--- pkg/helpers/service.go
+++ pkg/helpers/service.go
@@ -1,13 +1,16 @@
 package helpers
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/awslabs/aws-service-operator/pkg/config"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // CreateExternalNameService will create a Kubernetes Servic Using ExternalName types
-func CreateExternalNameService(config config.Config, resource interface{}, svcName string, svcNamespace string, externalNameTemplate string, svcPort int32) string {
+func CreateExternalNameService(config config.Config, resource interface{}, svcName string, svcNamespace string, externalNameTemplate string, svcPortTemplate string) string {
 	logger := config.Logger
 
 	externalName, err := Templatize(externalNameTemplate, Data{Obj: resource, Config: config})
@@ -15,6 +18,23 @@ func CreateExternalNameService(config config.Config, resource interface{}, svcNa
 		logger.WithError(err).Error("error parsing external name template")
 		return ""
 	}
+
+	svcPortString, err := Templatize(svcPortTemplate, Data{Obj: resource, Config: config})
+	if err != nil {
+		logger.WithError(err).Error("error parsing service port template")
+		return ""
+	}
+
+	svcPortStripSlash := strings.Replace(svcPortString, "\\", "", -1)
+
+	svcPortInt64, err := strconv.ParseInt(svcPortStripSlash, 0, 16)
+	if err != nil {
+		logger.WithError(err).Error("error converting service port template string to int")
+		return ""
+	}
+
+	// ParseInt only returns an int64, must convert to int32 for apiv1.ServicePort field
+	svcPort := int32(svcPortInt64)
 
 	service := &apiv1.Service{
 		ObjectMeta: metav1.ObjectMeta{
